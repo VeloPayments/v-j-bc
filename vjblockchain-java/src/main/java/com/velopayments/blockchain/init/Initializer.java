79576c65dfcb22517cc62103f8cc08fd406d9dad
Justin Handville <nanolith@gmail.com>
2018-06-26 20:20:23 -0400

    Package native .so with jar; extract at runtime.

--- vjblockchain-java/src/main/java/com/velopayments/blockchain/init/Initializer.java
+++ vjblockchain-java/src/main/java/com/velopayments/blockchain/init/Initializer.java
@@ -1,5 +1,7 @@
 package com.velopayments.blockchain.init;
 
+import java.io.*;
+
 public final class Initializer {
 
     /**
@@ -13,12 +15,46 @@ public final class Initializer {
      * application initialization.
      */
     public static synchronized void init() {
-        System.loadLibrary("vjblockchain-native");
+        if (!loaded) {
+            InputStream is = null;
+            OutputStream os = null;
+            File libFile = null;
 
-        /* attempt to initialize vjblockchain */
-        if (!blockchainInit()) {
-            /* unspecified error that couldn't be thrown. */
-            throw new IllegalStateException("Bad vjblockchain registration.");
+            try {
+                is =
+                    Initializer.class
+                       .getResourceAsStream("/libvjblockchain-native.so");
+                libFile = File.createTempFile("lib", ".so");
+                os = new FileOutputStream(libFile);
+                byte[] buffer = new byte[16384];
+                int length;
+
+                while ((length = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, length);
+                }
+
+                System.load(libFile.getPath());
+
+                /* attempt to initialize vjblockchain */
+                if (!blockchainInit()) {
+                    /* unspecified error that couldn't be thrown. */
+                    throw new IllegalStateException("Bad vjblockchain registration.");
+                }
+
+                loaded = true;
+            } catch (IOException e) {
+                throw new IllegalStateException("Could not load class lib", e);
+            } finally {
+                if (null != is) {
+                    try { is.close(); } catch (IOException e) { }
+                }
+                if (null != os) {
+                    try { os.close(); } catch (IOException e) { }
+                }
+                if (null != libFile) {
+                    libFile.deleteOnExit();
+                }
+            }
         }
     }
 
@@ -28,4 +64,9 @@ public final class Initializer {
      * @return true on success.  Returns false or throws exception on failure.
      */
     private static native boolean blockchainInit();
+
+    /**
+     * Set to true the first time this is loaded.
+     */
+    private static boolean loaded = false;
 }
