087b82238e5117d81045490164745d37a3ba797b
Justin Handville <nanolith@gmail.com>
2018-07-06 16:50:03 -0400

    Examples initial revision.

---
+++ vjblockchain-example-client-consumer/src/main/java/com/velopayments/blockchain/example/consumer/Consumer.java
@@ -0,0 +1,129 @@
+package com.velopayments.blockchain.example.consumer;
+
+import com.velopayments.blockchain.cert.*;
+import com.velopayments.blockchain.client.*;
+import com.velopayments.blockchain.crypt.*;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class Consumer implements Runnable {
+
+    public void run() {
+        AgentConnection conn = null;
+
+        try {
+            createDirectoryIfMissing();
+
+            conn =
+                new AgentConnection(
+                    blockDBDir.toFile().getPath(),
+                    consumerEntityId, consumerPrivateKey);
+
+            readCerts(conn);
+        } catch (IOException e) {
+            System.out.println("Got IOException.");
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Got IOException closing Agent Connection");
+            }
+        }
+    }
+
+    /**
+     * Main entry point.
+     */
+    public static void main(String[] args) {
+        new Consumer().run();
+    }
+
+    /**
+     * Create the blockdb directory.
+     */
+    private void createDirectoryIfMissing() throws IOException {
+        if (Files.notExists(blockDBDir)) {
+            Files.createDirectories(blockDBDir);
+        }
+    }
+
+    /**
+     * Read certificates from blockchain.
+     */
+    private void readCerts(AgentConnection conn) throws IOException {
+        UUID lastBlockId = ROOT_UUID;
+
+        for (;;) {
+            Optional<UUID> nextBlockId = conn.getNextBlockId(lastBlockId);
+            if (nextBlockId.isPresent()) {
+                readBlock(conn, nextBlockId.orElse(ROOT_UUID));
+                lastBlockId = nextBlockId.orElse(ROOT_UUID);
+            } else {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+
+    /**
+     * Read the provided block.
+     */
+    private void
+    readBlock(AgentConnection conn, UUID blockId) throws IOException {
+        byte[] block =
+            conn.getBlockById(blockId).orElseThrow(
+                () -> new IOException("Could not read block " + blockId));
+
+        CertificateReader reader =
+            new CertificateReader(new CertificateParser(block));
+
+        for (int i = 0; i < reader.count(Field.WRAPPED_TRANSACTION_TUPLE);
+             ++i) {
+
+            try {
+                readTransaction(
+                    reader.get(
+                        Field.WRAPPED_TRANSACTION_TUPLE, i).asByteArray());
+            } catch (MissingFieldException e) {
+                System.out.println("Missing transaction " + Integer.valueOf(i));
+            }
+        }
+    }
+
+    /**
+     * Read the provided transaction.
+     */
+    private void
+    readTransaction(byte[] transaction) {
+        try {
+            CertificateReader reader =
+                new CertificateReader(new CertificateParser(transaction));
+
+            UUID transactionID = reader.getFirst(Field.CERTIFICATE_ID).asUUID();
+            String message = reader.getFirst(0x0401).asString();
+
+            System.out.println(transactionID + ": " + message);
+
+        } catch (Throwable e) {
+            System.out.println("Could not parse transaction.");
+        }
+    }
+
+    private static final Path blockDBDir = Paths.get("blockdb");
+    /* TODO - pull entity, encryption keypair, and signing keypair from private
+     * cert. */
+    private static final UUID consumerEntityId =
+        UUID.fromString("10150a08-94f9-4351-a32a-8075aa65ad3b");
+    private static final UUID ROOT_UUID =
+        UUID.fromString("00000000-0000-0000-0000-000000000000");
+    private static final EncryptionPrivateKey consumerPrivateKey =
+        new EncryptionPrivateKey(new byte[] { 0,  1,  2,  3,  4,  5,  6,  7,
+                                              8,  9, 10, 11, 12, 13, 14, 15,
+                                             16, 17, 18, 19, 20, 21, 22, 23,
+                                             24, 25, 26, 27, 28, 29, 30, 31 });
+}
---
+++ vjblockchain-example-client-producer/src/main/java/com/velopayments/blockchain/example/producer/Producer.java
@@ -0,0 +1,94 @@
+package com.velopayments.blockchain.example.producer;
+
+import com.velopayments.blockchain.cert.CertificateBuilder;
+import com.velopayments.blockchain.cert.Field;
+import com.velopayments.blockchain.client.AgentConnection;
+import com.velopayments.blockchain.crypt.EncryptionPrivateKey;
+import java.io.*;
+import java.nio.file.*;
+import java.util.UUID;
+
+public class Producer implements Runnable {
+
+    public void run() {
+        AgentConnection conn = null;
+
+        try {
+            createDirectoryIfMissing();
+
+            conn =
+                new AgentConnection(
+                    blockDBDir.toFile().getPath(),
+                    producerEntityId, producerPrivateKey);
+
+            writeCerts(conn);
+
+        } catch (IOException e) {
+            System.out.println("Got IOException.");
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Got IOException closing Agent Connection");
+            }
+        }
+    }
+
+    /**
+     * Main entry point.
+     */
+    public static void main(String[] args) {
+        new Producer().run();
+    }
+
+    /**
+     * Create the blockdb directory.
+     */
+    private void createDirectoryIfMissing() throws IOException {
+        if (Files.notExists(blockDBDir)) {
+            Files.createDirectories(blockDBDir);
+        }
+    }
+
+    /**
+     * Write certificates to blockchain.
+     */
+    private void writeCerts(AgentConnection conn) throws IOException {
+        for (int i = 0; i < 1000; ++i) {
+            CertificateBuilder builder =
+                CertificateBuilder.createCertificateBuilder(
+                    EXAMPLE_TRANSACTION_TYPE);
+
+            builder.addUUID(
+                Field.CERTIFICATE_ID, UUID.randomUUID());
+            builder.addString(
+                0x0401, "Example transaction " + Integer.valueOf(i));
+
+            System.out.println("Submitting transaction " + Integer.valueOf(i));
+
+            conn.submit(builder.emit());
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+            }
+        }
+
+        conn.commitTransactions();
+    }
+
+    private static final UUID EXAMPLE_TRANSACTION_TYPE =
+        UUID.fromString("c1625721-4930-4b7a-b321-8386285330f4");
+    private static final Path blockDBDir = Paths.get("blockdb");
+    /* TODO - pull entity, encryption keypair, and signing keypair from private
+     * cert. */
+    private static final UUID producerEntityId =
+        UUID.fromString("10150a08-94f9-4351-a32a-8075aa65ad3b");
+    private static final EncryptionPrivateKey producerPrivateKey =
+        new EncryptionPrivateKey(new byte[] { 0,  1,  2,  3,  4,  5,  6,  7,
+                                              8,  9, 10, 11, 12, 13, 14, 15,
+                                             16, 17, 18, 19, 20, 21, 22, 23,
+                                             24, 25, 26, 27, 28, 29, 30, 31 });
+}
